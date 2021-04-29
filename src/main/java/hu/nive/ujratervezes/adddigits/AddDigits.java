3964153516415f3f6ca2a5b19b63f531311cd7da
Gergő Csongor Vincze <65530422+Asu-Tia@users.noreply.github.com>
2021-04-29 10:16:46 +0200

    Complete task.

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,19 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        if (input == null || input.equals("")) {
+            return -1;
+        }
+        char[] digits = input.toCharArray();
+        int result = 0;
+        for (char digit : digits) {
+            try {
+                result += Integer.parseInt(String.valueOf(digit));
+            } catch (NumberFormatException ignored) {
+            }
+        }
+        return result;
+    }
 }
